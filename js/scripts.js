22cc34c59340f31fa25baa7309fc62ec44a61c19
Michael Watts <wattsjmichael@gmail.com>
2020-08-10 11:02:11 -0700

    adds subtraction/multiplication/division functions
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 function add(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function division(number1, number2) {
+  return number1 / number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter a number:"));
-alert(add(number1, number2));
+const number2 = parseInt(prompt("Enter another number:"));
+
+
+const result = division(number1, number2);
+alert(result);
